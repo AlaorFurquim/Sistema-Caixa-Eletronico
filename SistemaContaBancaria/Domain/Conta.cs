d0a1cb3a7fac41efbc6403a9e324ecf9bc1d50a7
AlaorFurquim <79509487+AlaorFurquim@users.noreply.github.com>
2021-11-08 22:18:56 -0400

    Arrumando o projeto

--- SistemaContaBancaria/Domain/Conta.cs
+++ SistemaContaBancaria/Domain/Conta.cs
@@ -9,16 +9,12 @@ namespace SistemaContaBancaria.Domain
 {
     public class Conta
     {
-       public Conta(string nome, string cpf,string numero)
+        public Conta(string nome, string cpf, string numero)
         {
             this.Nome = nome;
             this.CPF = cpf;
             this.Numero = numero;
         }
-
-        
-
-
         public Conta(IDataReader dr)
         {
             IdConta = (int)dr["idConta"];
@@ -34,15 +30,7 @@ namespace SistemaContaBancaria.Domain
         public string Nome { get; private set; }
         public string CPF { get; private set; }
         public string Numero { get; private set; }
-
-        internal void ValidarCpf()
-        {
-            throw new NotImplementedException();
-        }
-
         public decimal Saldo { get; private set; }
-
-
 
        public void Deposito(decimal valor)
         {
@@ -68,41 +56,7 @@ namespace SistemaContaBancaria.Domain
                 throw new Exception("O campo numero deve ser preenchido");
         }
 
-        public bool ValidarCpf(string cpf)
-        {
-            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-            string tempCpf;
-            string digito;
-            int soma;
-            int resto;
-            cpf = cpf.Trim();
-            cpf = cpf.Replace(".", "").Replace("-", "");
-            if (cpf.Length != 11)
-                return false;
-            tempCpf = cpf.Substring(0, 9);
-            soma = 0;
-
-            for (int i = 0; i < 9; i++)
-                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
-            resto = soma % 11;
-            if (resto < 2)
-                resto = 0;
-            else
-                resto = 11 - resto;
-            digito = resto.ToString();
-            tempCpf = tempCpf + digito;
-            soma = 0;
-            for (int i = 0; i < 10; i++)
-                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
-            resto = soma % 11;
-            if (resto < 2)
-                resto = 0;
-            else
-                resto = 11 - resto;
-            digito = digito + resto.ToString();
-            return cpf.EndsWith(digito);
-        }
+        
 
         public override string ToString()
         {
